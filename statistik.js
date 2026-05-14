0a64ded11a32b8840a28a1595289596e629dfb1d
anugerahlouis apriliano dev <anugerahlouisapriliano@gmail.com>
2026-05-14 10:26:33 +0700

    Add async script loading for GoatCounter

---
+++ statistik.js
@@ -0,0 +1,7 @@
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = '//gc.zgo.at/count.js';
+    script.setAttribute('data-goatcounter', 'https://anugrahlouis.goatcounter.com/count');
+    document.head.appendChild(script);
+})();
